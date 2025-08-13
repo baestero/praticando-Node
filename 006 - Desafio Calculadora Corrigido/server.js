1f4e816c380d4295be1de50f8c87bef01ac5afa2
baestero <leonardobaestero@gmail.com>
2025-08-13 18:29:54 -0300

    Desafio da Calculadora Corrigido

--- 006 - Desafio Calculadora Corrigido/server.js
+++ 006 - Desafio Calculadora Corrigido/server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Jesus te ama");
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+  const resultado = +req.body.num1 + +req.body.num2;
+  res.send(`O resultado é: ${resultado}`);
+});
+
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(`${__dirname}/bmiCalculator.html`);
+});
+
+app.post("/bmiCalculator", (req, res) => {
+  const peso = parseFloat(req.body.peso);
+  const altura = parseFloat(req.body.altura);
+
+  const resultado = peso / (altura * altura);
+
+  res.send(`Seu IMC é: ${resultado.toFixed(2)}`);
 });
 
 app.listen(3000, () => {
